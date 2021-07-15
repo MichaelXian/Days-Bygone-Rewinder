7f564a1bd930f772c615f0c464d501903fcc5d8d
MichaelXian <michaelkxian@gmail.com>
2021-07-15 11:12:10 -0700

    initial commit

---
+++ Rewind.sikuli/Rewind.py
@@ -0,0 +1,7 @@
+import time
+time.sleep(0)
+screen = Region(0,0,1920,1080)
+elixirTab = "1626372344232.png"
+rewind = Pattern("1626372385701.png").similar(0.90)
+battle = "battle.png"
+campaign = "1626372534941.png"
